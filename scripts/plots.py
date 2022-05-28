0b4c83b8c9de3bd372d1a9772a3ae0788997f089
TamarLivne <livneta@post.bgu.ac.il>
2022-05-28 13:04:18 +0300

    # divide the data per trail per stimuli and calculate mean per stimuli and making the length of the trails constant

--- scripts/plots.py
+++ scripts/plots.py
@@ -4,14 +4,7 @@ import matplotlib.pyplot as plt
 import pickle
 from pandas import read_csv
 
-fs = 256  # (Hz)
-# elec = 13
-# trials_amount = 15
-# stimuli = 30
-# labels_amount = 3
-# trials = np.random.randint(0, 100, (trials_amount, elec, fs * stimuli))
-# labels = np.random.randint(low=0, high=labels_amount, size=(trials_amount, stimuli))
-# trials = np.reshape(trials, (trials_amount, elec, fs, stimuli))
+
 
 lowcut = 1
 highcut = 40
@@ -27,7 +20,7 @@ with open("..\\recordings\\34\\targets.pickle", "rb") as f:
 #  option for filter function from mne
 # and we already have a function that do so in th eeg.py -> fulter_data
 data = trials[0]
-data.filter(l_freq=low_pass, h_freq=high_pass, verbose=False)
+#data.filter(l_freq=low_pass, h_freq=high_pass, verbose=False)#tamar: is this ok? why only on data?
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -43,22 +36,50 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     y = lfilter(b, a, data)
     return y
 
+fs = 256  # (Hz)
+elec = len(trials[0][0])
+trials_amount = len(trials)
+stimuli = len(trials[1])
+labels_amount = max(labels[0])+1
+# trials = np.random.randint(0, 100, (trials_amount, elec, fs * stimuli))
+# labels = np.random.randint(low=0, high=labels_amount, size=(trials_amount, stimuli))
+# trials = np.reshape(trials, (trials_amount, elec, fs, stimuli))
 
-times = np.arange(256)
+# times = np.arange(256)
 # filtered = butter_bandpass_filter(trials, lowcut, highcut, fs, order=2)
-plt.subplot(121)
-plt.plot(times, trials[1, 1, 1, :])
-plt.xlabel('before')
-plt.subplot(122)
-# plt.plot(times, filtered[1, 1, :, 1])
-plt.xlabel('after')
-plt.show()
+# plt.subplot(121)
+# plt.plot(times, trials[1, 1, 1, :])
+# plt.xlabel('before')
+# plt.subplot(122)
+# # plt.plot(times, filtered[1, 1, :, 1])
+# plt.xlabel('after')
+# plt.show()
+
+# making the length of the trails constant
+trails_len = []
+for i in range(trials_amount):
+    for j in range(stimuli):
+        trails_len.append(np.shape(trials[i][j])[1])
+min_len = min(trails_len)
+for i in range(trials_amount):
+    for j in range(stimuli):
+        trials[i][j] = trials[i][j][:, 0:60]
+
 
 # divide the data per trail per stimuli and calculate mean per stimuli
-mean_trails = np.zeros((trials_amount, elec, fs, labels_amount))
-for i in range(trials_amount):
+mean_data = []
+for t in range(trials_amount):
+    data_set = trials[t]
+    labels_set = np.array((labels[t]))
+    mean_trials = []
     for j in range(labels_amount):
-        indx_trials = (labels[i, :] == 0)
-        m_trials = trials[i, :, :, indx_trials]
-        t_trials = np.mean(m_trials, 0)
-        mean_trails[i, :, :, j] = t_trials
+        labels_trails = []
+        for k in range(stimuli):
+            if labels_set[k] == j:
+                labels_trails.append(data_set[k])
+        mean_stimuli = np.zeros((len(labels_trails), 13, 60))
+        for d in range(len(labels_trails)):
+            mean_stimuli[d, :, :] = labels_trails[d]
+        mean_trials.append(np.mean(mean_stimuli, 0))
+    mean_data.append(mean_trials)
+
